9e85bed1cf58f21405c80b1a1f18cc006c45649c
aslam5176 <wasimaariz5176@gmail.com>
2022-06-22 11:52:58 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,40 @@
+import os
+
+##f = open("demo.txt", "r")
+
+##print(f.read())
+##print(f.read(2))
+##print(f.readline())
+##print(f.readline())
+
+##for i in f:
+##    print(i, "-------------------")
+
+##f.close()
+
+
+##f = open("demo.txt", "a")
+##
+####f = open("demo.txt", "w")
+##
+##f.write(" Naga")
+##
+##f.close()
+##
+##r = open("demo.txt", "r")
+##
+##print(r.read())
+##
+##r.close()
+
+##The “r+” and “rw+” are the same.
+##...
+##r+: Opens a file in read and write mode. ...
+##w+: Opens a file in read and write mode. ...
+##rw+: Opens a file in read and write mode.
+
+
+##os.remove("1.py")
+
+
+
